2159b4f2280428f11f19c59c48bd1a759d7a9216
Patrik Erdes <patrik@erdes.se>
2018-03-15 08:36:31 +0100

    Cut down on the number of gradle versions to improve build time

--- src/test/groovy/se/patrikerdes/GradleVersionFunctionalTest.groovy
+++ src/test/groovy/se/patrikerdes/GradleVersionFunctionalTest.groovy
@@ -36,16 +36,8 @@ class GradleVersionFunctionalTest extends BaseFunctionalTest {
         where:
         gradleVersion << [
                 '2.8',
-                '2.9',
-                '2.10',
-                '2.11',
-                '2.13',
                 '2.14',
                 '3.0',
-                '3.1',
-                '3.2',
-                '3.3',
-                '3.4',
                 '3.5',
                 '4.0',
                 '4.1',
