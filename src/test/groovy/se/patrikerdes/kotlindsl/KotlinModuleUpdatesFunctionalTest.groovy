88b06e27e716b2d04778683e015b8c591f29912f
Thomas <hollandpirates@gmail.com>
2018-04-12 17:40:39 +0200

    Add module updates functional test

---
+++ src/test/groovy/se/patrikerdes/kotlindsl/KotlinModuleUpdatesFunctionalTest.groovy
@@ -0,0 +1,86 @@
+package se.patrikerdes.kotlindsl
+
+import se.patrikerdes.CurrentVersions
+
+class KotlinModuleUpdatesFunctionalTest extends KotlinBaseFunctionalTest {
+    void "an outdated module dependency with a fixed version can be updated, string notation"() {
+        given:
+        buildFile << """
+            plugins {
+                application
+                java
+                id("se.patrikerdes.use-latest-versions")
+                id("com.github.ben-manes.versions") version "$CurrentVersions.VERSIONS"
+            }
+            
+            repositories {
+                mavenCentral()
+            }
+            
+            dependencies {
+                testCompile("junit:junit:4.0")
+            }
+        """
+
+        when:
+        useLatestVersions()
+        String updatedBuildFile = buildFile.getText('UTF-8')
+
+        then:
+        updatedBuildFile.contains("junit:junit:$CurrentVersions.JUNIT")
+    }
+
+    void "an outdated module dependency with a version range can be updated"() {
+        given:
+        buildFile << """
+            plugins {
+                application
+                java
+                id("se.patrikerdes.use-latest-versions")
+                id("com.github.ben-manes.versions") version "$CurrentVersions.VERSIONS"
+            }
+            
+            repositories {
+                mavenCentral()
+            }
+            
+            dependencies {
+                testCompile("junit:junit:[3.0,4.0]")
+            }
+        """
+
+        when:
+        useLatestVersions()
+        String updatedBuildFile = buildFile.getText('UTF-8')
+
+        then:
+        updatedBuildFile.contains('junit:junit:4.12')
+    }
+
+    void "a module dependency without a fixed version which is up to date is not updated"() {
+        given:
+        buildFile << """
+            plugins {
+                application
+                java
+                id("se.patrikerdes.use-latest-versions")
+                id("com.github.ben-manes.versions") version "$CurrentVersions.VERSIONS"
+            }
+            
+            repositories {
+                mavenCentral()
+            }
+            
+            dependencies {
+                testCompile("junit:junit:4+")
+            }
+        """
+
+        when:
+        useLatestVersions()
+        String updatedBuildFile = buildFile.getText('UTF-8')
+
+        then:
+        updatedBuildFile.contains('junit:junit:4+')
+    }
+}
