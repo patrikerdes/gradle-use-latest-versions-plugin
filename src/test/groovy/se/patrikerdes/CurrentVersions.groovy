940ab363906fddaae4d89db0ceb691ede7345c57
Patrik Erdes <patrik@erdes.se>
2018-10-24 19:45:36 +0200

    Set the correct current versions

--- src/test/groovy/se/patrikerdes/CurrentVersions.groovy
+++ src/test/groovy/se/patrikerdes/CurrentVersions.groovy
@@ -1,7 +1,7 @@
 package se.patrikerdes
 
 class CurrentVersions {
-    public static final String VERSIONS = '0.17.0'
+    public static final String VERSIONS = '0.20.0'
     public static final String JUNIT = '4.12'
     public static final String LOG4J = '1.2.17'
 }
