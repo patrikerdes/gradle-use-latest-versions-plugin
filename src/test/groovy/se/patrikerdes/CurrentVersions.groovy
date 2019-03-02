8bba5f675beb1def73e76581e3673eaf7d3e347c
Patrik Erdes <patrik.erdes@greenbyte.com>
2019-03-02 18:34:10 +0100

    com.github.ben-manes.versions version 0.21.0
    * Update unit tests to use it
    * Update unit tests to expect updates of versions to update to it (which should make unit tests pass again)
    * Update README to use it
    * Update README to say that it is supported

--- src/test/groovy/se/patrikerdes/CurrentVersions.groovy
+++ src/test/groovy/se/patrikerdes/CurrentVersions.groovy
@@ -1,7 +1,7 @@
 package se.patrikerdes
 
 class CurrentVersions {
-    public static final String VERSIONS = '0.20.0'
+    public static final String VERSIONS = '0.21.0'
     public static final String JUNIT = '4.13-beta-2'
     public static final String LOG4J = '1.2.17'
 }
