1ec6663e317d57c534718143045e80e87dc8ce72
Patrik Erdes <patrik.erdes@greenbyte.com>
2019-05-23 20:55:09 +0200

    Update JUnit version (fix unit tests)

--- src/test/groovy/se/patrikerdes/CurrentVersions.groovy
+++ src/test/groovy/se/patrikerdes/CurrentVersions.groovy
@@ -2,6 +2,6 @@ package se.patrikerdes
 
 class CurrentVersions {
     public static final String VERSIONS = '0.21.0'
-    public static final String JUNIT = '4.13-beta-2'
+    public static final String JUNIT = '4.13-beta-3'
     public static final String LOG4J = '1.2.17'
 }
