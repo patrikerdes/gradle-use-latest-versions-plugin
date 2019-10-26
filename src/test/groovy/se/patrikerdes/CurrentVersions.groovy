f7ea3ad82b0e6f25841ae9d257c3915526d30833
Patrik Erdes <patrik@erdes.se>
2019-10-26 21:15:55 +0200

    New junit version

--- src/test/groovy/se/patrikerdes/CurrentVersions.groovy
+++ src/test/groovy/se/patrikerdes/CurrentVersions.groovy
@@ -2,6 +2,6 @@ package se.patrikerdes
 
 class CurrentVersions {
     public static final String VERSIONS = '0.27.0'
-    public static final String JUNIT = '4.13-beta-3'
+    public static final String JUNIT = '4.13-rc-1'
     public static final String LOG4J = '1.2.17'
 }
