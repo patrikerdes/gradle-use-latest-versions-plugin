f7e1dafad94f8b72b1a94504d9a9d551fa2963b0
Patrik Erdes <patrik@erdes.se>
2021-05-30 14:54:05 +0200

    Update current version of versions plugin

--- src/test/groovy/se/patrikerdes/CurrentVersions.groovy
+++ src/test/groovy/se/patrikerdes/CurrentVersions.groovy
@@ -1,7 +1,7 @@
 package se.patrikerdes
 
 class CurrentVersions {
-    public static final String VERSIONS = '0.38.0'
+    public static final String VERSIONS = '0.39.0'
     public static final String JUNIT = '4.13.2'
     public static final String JUNIT_DEPS = '4.11'
     public static final String LOG4J = '1.2.17'
